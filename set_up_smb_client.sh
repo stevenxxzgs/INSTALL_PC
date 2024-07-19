2b075591a634847c0ac10060b9bc8b8612608dbd
stevenxxzg <stevenxxzgs@outlook.com>
2024-07-19 12:21:07 +0800

    update: 把源主机ip和目的主机的地址都抽出来作为变量

--- set_up_smb_client.sh
+++ set_up_smb_client.sh
@@ -1,6 +1,13 @@
 ## 使用/mnt/remote_folder去共享远程主机的文件
 
-sudo mkdir /mnt/remote_folder
-sudo chown sportvision:sportvision /mnt/remote_folder
+# sudo mkdir /mnt/remote_folder
+# sudo chown sportvision:sportvision /mnt/remote_folder
+# sudo apt install smbclient -y
+# sudo mount -t cifs //172.22.2.162/shared_folder /mnt/remote_folder -o username=sportvision,password=sv04091211,rw
+
+remote_folder="/mnt/remote_folder"
+remote_host="172.22.2.162"
+sudo mkdir "$remote_folder"
+sudo chown sportvision:sportvision "$remote_folder"
 sudo apt install smbclient -y
-sudo mount -t cifs //172.22.2.162/shared_folder /mnt/remote_folder -o username=sportvision,password=sv04091211,rw
+sudo mount -t cifs "//$remote_host/shared_folder" "$remote_folder" -o username=sportvision,password=sv04091211,rw
